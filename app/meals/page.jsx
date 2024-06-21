2761f46efdd279d4dc9246dc4bd8e792b9e6bd0d
Kornel0226 <r6kory@gmail.com>
2024-06-21 17:14:16 +0200

    added Suspense for fetching and some Errors

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,24 +2,35 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-const MealsPage = async () => {
-
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+const MealsPage = () => {
   return (
     <>
-    <header className={classes.header}>
-      <h1>
-        Delicous meals, created <span className={classes.highlight}>by you</span>
-      </h1>
-      <p>
-        Choose your favourite recipe and cook it yourself, It is easy and fun.
-      </p>
-      <p className={classes.cta}><Link href="/meals/share">Share your favourite recipe.</Link></p>
-    </header>
-    <main className={classes.main}>
-      <MealsGrid meals={meals}/>
-    </main>
+      <header className={classes.header}>
+        <h1>
+          Delicous meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself, It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe.</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
     </>
   );
 };
