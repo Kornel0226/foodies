7f0e197f095306c9438397f9026bb0240ed92239
kkory02 <r6kory@gmail.com>
2024-06-29 12:44:24 +0200

    meal not found page added

--- app/meals/[meal]/page.jsx
+++ app/meals/[meal]/page.jsx
@@ -1,9 +1,36 @@
-const MealPage = ({ params }) => {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+const MealPage = ({params}) => {
+
+  const meal = getMeal(params.meal);
+
+  if(!meal){
+    return notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />")
+
   return (
-    <main>
-      <h1>Meal Page</h1>
-      <p>{params.meal}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+          <div className={classes.image}>
+             <Image src={meal.image} alt={meal.title} fill />
+          </div>
+          <div className={classes.headerText}>
+            <h1>{meal.title}</h1>
+            <p className={classes.creator}>
+              By <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+            </p>
+            <p className={classes.summary}>{meal.summary}</p>
+          </div>
+      </header>
+      <main>
+          <p className={classes.instructions} dangerouslySetInnerHTML={{__html: meal.instructions,}}></p>
+      </main>
+    </>
   );
 };
 
